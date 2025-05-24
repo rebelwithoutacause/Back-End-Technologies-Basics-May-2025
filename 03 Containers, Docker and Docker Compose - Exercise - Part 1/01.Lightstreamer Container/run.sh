2c7ff10a97a0c3ca8ff03bc4fa6c04ee6a583082
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-05-24 13:37:18 +0300

    Delete 03 Containers, Docker and Docker Compose - Exercise - Part 1 directory

--- 03 Containers, Docker and Docker Compose - Exercise - Part 1/01.Lightstreamer Container/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run Lightstreamer in Docker
-docker run -d --name ls-server -p 80:8080 lightstreamer:latest
--- 03 Containers, Docker and Docker Compose - Exercise - Part 1/02.Ghost Container/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run Ghost in Docker
-docker run -d --name ghost-container -p 3001:2368 -e NODE_ENV=development ghost:latest
