22664cc1ab98db254f19a644fa7b3e3e17e17173
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-05-24 13:34:58 +0300

    Delete Exercise Containers, Docker and Docker Compose - Part 1 directory

--- Exercise Containers, Docker and Docker Compose - Part 1/04.SQL Server Container/sqlserver-docker/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run SQL Server in Docker
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=YourStrong!Passw0rd" \
-    -p 1433:1433 --name sql-container \
-    -v sqldata:/var/opt/mssql \
-    -d mcr.microsoft.com/mssql/server
