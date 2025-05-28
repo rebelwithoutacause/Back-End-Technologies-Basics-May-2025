691e8e9695db92d2c5a02b1c25911ecb680c8ca0
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-05-28 13:34:13 +0300

    Delete 04. Containers, Docker and Docker Compose - Part 2/01.MariaDB/MariaDB Client and Server in a Network/mariadb-network-setup directory

--- 04. Containers, Docker and Docker Compose - Part 2/01.MariaDB/MariaDB Client and Server in a Network/mariadb-network-setup/mariadb-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Connect to MariaDB Server via client container
-docker run -it --rm \
-  --name mariadb_client \
-  --network mariadb_net \
-  mariadb \
-  mariadb -h mariadb_server -u myuser -p
